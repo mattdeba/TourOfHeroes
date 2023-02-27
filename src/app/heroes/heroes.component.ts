b8eb8c747a5e0aac9b5b427232a37429c682afdc
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-27 17:04:32 +0100

    4

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero'
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
+
+  getHeroes(): void{
+    /**
+     * La nouvelle version attend que l'Observable émette le tableau de héros,
+     * ce qui peut arriver maintenant ou dans plusieurs minutes.
+     * La méthode subscribe() transmet le tableau émis à la callback,
+     * qui définit la propriété heroes du composant.
+     */
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from "../message.service";
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+/**
+  La propriété messageService doit être publique
+  car nous allons la lier au modèle.
+**/
+  constructor(public messageService: MessageService) { }
+
+  ngOnInit(): void {
+  }
+
+}
